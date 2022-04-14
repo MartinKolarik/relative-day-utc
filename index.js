9ff61a374f7dca3e4e692ef85e01250e489ce1c2
Martin Kolárik <martin@kolarik.sk>
2022-04-14 22:51:43 +0200

    Add type annotations

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * @param {number} [addDays=0]
+ * @param {Date|string} [sinceDate]
+ * @returns {Date}
+ */
 module.exports = (addDays = 0, sinceDate) => {
 	let date = sinceDate ? new Date(sinceDate) : new Date();
 	date.setUTCHours(0, 0, 0, 0);
